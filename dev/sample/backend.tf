6891465925680a16e7ce7ec1e10e6a9ad98e7e9f
xwen11 <102186192+xwen11@users.noreply.github.com>
2024-08-09 09:26:24 -0700

    Create files

---
+++ dev/sample/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testIaC"
+    storage_account_name = "storagewen"
+    container_name       = "containerwen"
+    key                  = "sample.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "9688b0ae-bd2c-497a-adb7-155619d7ccda"
+  }
+}
