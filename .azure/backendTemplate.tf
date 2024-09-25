10880bd7fdcd4461e885dc84151c9f94ff78d370
xwen11 <102186192+xwen11@users.noreply.github.com>
2024-09-24 17:30:07 -0700

    Create files

--- .azure/backendTemplate.tf
+++ .azure/backendTemplate.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "testIaC"
-    storage_account_name = "storagewen"
-    container_name       = "containerwen"
+    resource_group_name  = "testingIaCWen"
+    storage_account_name = "ttttt2"
+    container_name       = "storetf"
     key                  = "{{.GroupName}}.tfstate"
     use_azuread_auth     = true
-    subscription_id      = "9688b0ae-bd2c-497a-adb7-155619d7ccda"
+    subscription_id      = "de3c4d5e-af08-451a-a873-438d86ab6f4b"
   }
 }
